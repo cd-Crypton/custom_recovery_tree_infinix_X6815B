db9ac1058da788b12810991393ce4a289c4a8902
Carlo Dee <carlodee.official@gmail.com>
2022-11-30 10:26:00 +0800

    Remove bootcontrol hal packages.

--- device.mk
+++ device.mk
@@ -14,12 +14,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl-1.1-mtkimpl.recovery \
     android.hardware.boot@1.0-impl-1.1-mtkimpl
 
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
     fastbootd \
  
 #PRODUCT_PACKAGES += \
